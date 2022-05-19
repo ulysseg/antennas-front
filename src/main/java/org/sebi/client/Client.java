246a098fba1a95e00ee9d7a4afa5eaecd27d086d
Ulysse Goarant <ulysse.goarant@capgemini.com>
2022-05-19 13:52:04 +0200

    get data from backend

---
+++ src/main/java/org/sebi/client/Client.java
@@ -0,0 +1,17 @@
+package org.sebi.client;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.sebi.incident.Incident;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.util.List;
+
+@Path("/rest/incidents")
+@RegisterRestClient
+public interface Client {
+
+    @GET
+    List<Incident> getIncidents(@QueryParam("APIKEY") String key);
+}
--- src/main/java/org/sebi/incident/IncidentResource.java
+++ src/main/java/org/sebi/incident/IncidentResource.java
@@ -1,5 +1,9 @@
 package org.sebi.incident;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.sebi.client.Client;
+
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -7,9 +11,15 @@ import javax.ws.rs.Path;
 
 @Path("rest/incidents")
 public class IncidentResource {
+
+    @RestClient
+    Client client;
+
+    @ConfigProperty(name = "org.sebi.incident.incidentservice.mp.rest.url")
+    String key;
     
     @GET
     public List<Incident> getIncidents(){
-       return Incident.listAll();
+       return client.getIncidents(key);
     } 
 }
