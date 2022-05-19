894073c1332fff6f4324cf651b2b8875e39c2779
Ulysse Goarant <ulysse.goarant@capgemini.com>
2022-05-19 15:01:57 +0200

    add a fallback response

--- src/main/java/org/sebi/incident/IncidentResource.java
+++ src/main/java/org/sebi/incident/IncidentResource.java
@@ -1,6 +1,7 @@
 package org.sebi.incident;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.sebi.client.Client;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 @Path("rest/incidents")
 public class IncidentResource {
@@ -19,7 +21,12 @@ public class IncidentResource {
     String key;
     
     @GET
+    @Fallback(fallbackMethod = "fallbackIncidents")
     public List<Incident> getIncidents(){
        return client.getIncidents(key);
-    } 
+    }
+
+    public Response fallbackIncidents() {
+        return Response.ok("fallback").build();
+    }
 }
